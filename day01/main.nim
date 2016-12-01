77bb8101ee7ddeca4ffacd095c62d7a8434dc563
Barnaby Gray <barnaby@pickle.me.uk>
2016-12-01 07:59:35 +0000

    Rewrite nim day1

--- day01/main.nim
+++ day01/main.nim
@@ -1,23 +1,31 @@
-import re, sets, strutils
+import future, re, sets, strutils
+
+type Coord = tuple[x: int, y: int]
+
+proc walk(input: string, stopping: (me: Coord) -> bool): int =
+  var me: Coord = (x: 0, y: 0)
+  var dx, dy, t = 0
+  dy = -1
+
+  for m in findAll(input, re"[LR]\d+"):
+    case m[0]
+      of 'L': t = dx; dx = dy; dy = -t
+      else: t = dx; dx = -dy; dy = t
+    for i in 1..parseInt(m[1 .. m.high]):
+      me.x += dx
+      me.y += dy
+      if stopping(me):
+        return abs me.x + abs me.y
+
+  return abs me.x + abs me.y
 
 var input = readFile "input.txt"
-type Coord = tuple[x: int, y: int]
+proc neverStop(me: Coord): bool = false
+echo "Answer #1: ", walk(input, neverStop)
+
 var places = initSet[Coord]()
-var me: Coord = (x: 0, y: 0)
-var dx, dy, t = 0
-var first = true
-dy = -1
-for m in findAll(input, re"[LR]\d+"):
-  case m[0]
-    of 'L': t = dx; dx = dy; dy = -t
-    of 'R': t = dx; dx = -dy; dy = t
-    else: discard
-  for i in 1..parseInt(m[1 .. m.high]):
-    me.x += dx
-    me.y += dy
-    if places.contains(me) and first:
-      echo "Answer #2: ", abs me.x + abs me.y
-      first = false
-    else:
-      places.incl(me)
-echo "Answer #1: ", abs me.x + abs me.y
+proc visited(me: Coord): bool =
+  if places.contains(me): return true
+  places.incl(me)
+  return false
+echo "Answer #2: ", walk(input, visited)
