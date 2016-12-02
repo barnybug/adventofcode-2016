0963e68cd5a74489eafb7f755a23feba100ab0e7
Barnaby Gray <barnaby@pickle.me.uk>
2016-12-02 08:03:20 +0000

    Simpler day 2

--- day02/main.py
+++ day02/main.py
@@ -1,34 +1,20 @@
-def board_buttons(board):
-    buttons = {}
-    board = board.split('\n')
-    for i, row in enumerate(board):
-        for j, col in enumerate(row):
-            button = {}
-            if j > 0:
-                button['L'] = row[j-1]
-            if j < len(row)-1:
-                button['R'] = row[j+1]
-            if i > 0:
-                button['U'] = board[i-1][j]
-            if i < len(board)-1:
-                button['D'] = board[i+1][j]
-            button = {k: v for k, v in button.items() if v != ' '}
-            buttons[col] = button
-    return buttons
-
 def walk(board):
-    buttons = board_buttons(board)
+    moves = {'L': (-1, 0), 'R': (1, 0), 'U': (0, -1), 'D': (0, 1)}
     answer = ''
     for line in open('input.txt'):
-        pos = '5'
-        for ch in line:
-            if ch in buttons[pos]:
-                pos = buttons[pos][ch]
-        answer += pos
+        y = next(i for i, row in enumerate(board) if '5' in row)
+        x = board[y].index('5')
+        for ch in line.strip():
+            dx, dy = moves[ch]
+            nx = min(max(x + dx, 0), len(board[0])-1)
+            ny = min(max(y + dy, 0), len(board)-1)
+            if board[ny][nx] != ' ':
+                x, y = nx, ny
+        answer += board[y][x]
     return answer
 
-board1 = '123\n456\n789'
+board1 = ['123', '456', '789']
 print(walk(board1))
 
-board2 = '  1  \n 234 \n56789\n ABC \n  D  '
+board2 = ['  1  ',' 234 ','56789',' ABC ','  D  ']
 print(walk(board2))
