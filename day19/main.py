00b01957bdde3d32c379954d41d4e7f596598b54
Barnaby Gray <barnaby@pickle.me.uk>
2016-12-19 13:46:46 +0000

    Some faster solutions using deque (inspired for reddit)

--- day19/main.py
+++ day19/main.py
@@ -1,41 +1,33 @@
-from sortedcontainers import SortedList
+from collections import deque
+
+n = 3018458
 
 def answer1():
-    n = 3018458
-    elfs = [1 for i in range(n)]
-    i = 0
-    for _ in range(n-1):
-        try:
-            j = elfs.index(1, i+1)
-        except:
-            j = elfs.index(1, 0, i)
-        elfs[j] = 0
-        # print(elfs, i)
-
-        try:
-            i = elfs.index(1, j+1)
-        except:
-            i = elfs.index(1, 0, j)
-    return elfs.index(1)+1
+    elfs = deque(range(n, 0, -1))
+    while len(elfs) > 1:
+        x = elfs.pop()
+        elfs.pop()
+        elfs.appendleft(x)
+    return elfs[0]
 
 def answer2():
-    n = 3018458
-    elfs = SortedList(range(1, n+1))
-    i = 0
-    for l in range(n, 1, -1):
-        if l % 10000 == 0: print(l)
-        h = l//2
-        j = (i+h)%l
-        # a = elfs[i]
-        # b = elfs[j]
-        elfs.pop(j)
-        # print(elfs, a, 'from', b)
-        if j > i:
-            i = (i+1)%(l-1)
+    left = deque()
+    right = deque()
+    for i in range(1, n+1):
+        if i < n//2 + 1:
+            left.append(i)
         else:
-            i = i%(l-1)
+            right.appendleft(i)
 
-    return elfs[0]
+    while right:
+        if len(left) > len(right):
+            left.pop()
+        else:
+            right.pop()
+
+        right.appendleft(left.popleft())
+        left.append(right.pop())
+    return left[0]
 
 print('Answer #1:', answer1())
 print('Answer #2:', answer2())
